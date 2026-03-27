38865689cc2752fe3ffc84da762fa66fdc4af029
kdf_7 <francesco.defaveri1999@gmail.com>
2026-03-27 11:43:53 +0100

    changed the doc

--- src/helpers.py
+++ src/helpers.py
@@ -19,19 +19,27 @@ def load_gtr_corrector(
         corrector_model: str = "ielabgroup/vec2text_gtr-base-st_corrector", 
         **kwargs) -> Tuple[SentenceTransformer, vec2text.trainers.Corrector]:
     """
-    This method is an helper function used to instantiate the gtr-t5-base corrector used for the generation of the privatised text.
+    This method is an helper function used to instantiate the (gtr-t5-base) model, inversion and corrector used for the generation of the privatised text.
+    We suggest to use the same model for encoding and reconstruction: if you need to train your own corrector, follow the instructions in the vec2text repository and then simply replace the model name in the corrector_model argument.
+    The st_model_name can be replaced with any sentence transformer model, but we suggest to use gtr-t5-base for better performance of the obfuscation mechanism, as it is the same model trained as the corrector.
 
-    Args:
-        log (logging.Logger): logger to use for logging the loading process.
-        st_model_name (str, optional): name of the sentence transformer model to use for embedding. Defaults to "sentence-transformers/gtr-t5-base".
-        inversion_model (str, optional): name of the vec2text inversion model to use. Defaults to "ielabgroup/vec2text_gtr-base-st_inversion".
-        corrector_model (str, optional): name of the vec2text corrector model to use. Defaults to "ielabgroup/vec2text_gtr-base-st_corrector".
-        kwargs: additional keyword arguments.
+    :param log: logger to use for logging the loading process.
+    :type log: logging.Logger
+    :param st_model_name: name of the sentence transformer model to use for embedding. Defaults to "sentence-transformers/gtr-t5-base".
+    :type st_model_name: str, optional
+    :param inversion_model: name of the vec2text inversion model to use. Defaults to "ielabgroup/vec2text_gtr-base-st_inversion".
+    :type inversion_model: str, optional
+    :param corrector_model: name of the vec2text corrector model to use. Defaults to "ielabgroup/vec2text_gtr-base-st_corrector".
+    :type corrector_model: str, optional
+    :param kwargs: additional keyword arguments.
+    :type kwargs: additional keyword arguments
 
-    Returns:
-        Tuple[SentenceTransformer, vec2text.trainers.Corrector]: the loaded sentence transformer model and the vec2text corrector model.
-        SentenceTransformer: the loaded sentence transformer model.
-        vec2text.trainers.Corrector: the loaded vec2text corrector model.
+    :return: a tuple containing the loaded sentence transformer model and the vec2text corrector.
+    :rtype: Tuple[SentenceTransformer, vec2text.trainers.Corrector]
+
+    Example usage:
+    >>> log = create('mylogger')
+    >>> st_model, corrector = load_gtr_corrector(log)
     """
     # Loading the sentence transformer model used for encoding the input text
     try:
@@ -41,7 +49,7 @@ def load_gtr_corrector(
         log.error(f"Error loading sentence transformer model: {e}")
         raise e
     # Loading the vec2text inversion and corrector models, as implemented by Morris et al.
-    #security checks
+    # security checks
     assert inversion_model in ["jxm/gtr__nq__32", "ielabgroup/vec2text_gtr-base-st_inversion"]
     log.info(f"Loading vec2text inversion model: {inversion_model}")
     inversion_model = vec2text.models.InversionModel.from_pretrained(
@@ -75,7 +83,7 @@ def load_gtr_corrector(
         ),
     )
     
-    log.info("All models loaded.")
+    log.info("Corrector loaded.")
     return st_model, corrector
 
 def embed(
@@ -86,14 +94,23 @@ def embed(
     """
     Return (N, st_model.get_sentence_embedding_dimension()) float32 L2-normalised embeddings from st_model.
 
-    Args:
-        log (logging.Logger): logger to use for logging the embedding process.
-        st_model (SentenceTransformer): the sentence transformer model to use for embedding the input texts.
-        texts (List[str]): the list of input texts to embed, N is the number of input texts.
-        kwargs: additional keyword arguments.
+    :param log: logger to use for logging the embedding process.
+    :type log: logging.Logger
+    :param st_model: the sentence transformer model to use for embedding the input texts.
+    :type st_model: SentenceTransformer
+    :param texts: the list of input texts to embed, N is the number of input texts.
+    :type texts: List[str]
+    :param kwargs: additional keyword arguments.
+    :type kwargs: additional keyword arguments
 
-    Returns:
-        torch.Tensor: the embeddings of the input texts, as a tensor of shape (N, st_model.get_sentence_embedding_dimension()) and dtype float32. The embeddings are L2-normalised.
+    :return: a tensor containing the L2-normalised embeddings of the input texts.
+    :rtype: torch.Tensor
+
+    Example usage:
+    >>> log = create('mylogger')
+    >>> st_model, _ = load_gtr_corrector(log)
+    >>> texts = ["This is a test sentence.", "Another test sentence."]
+    >>> embeddings = embed(log, st_model, texts)
     """
     with torch.no_grad():
         embs = st_model.encode(
@@ -114,14 +131,19 @@ def load_pool_of_queries(
     """
     Load a pool of queries from a specified collection.
 
-    Args:
-        log (logging.Logger): logger to use for logging the loading process.
-        collection_name (str, optional): name of the collection to load queries from. Defaults to "msmarco-passage/train".
-        kwargs: additional keyword arguments.
+    :param log: logger to use for logging the loading process.
+    :type log: logging.Logger
+    :param collection_name: the name of the collection to load queries from. Defaults to "msmarco-passage/train".
+    :type collection_name: str, optional
+    :param kwargs: additional keyword arguments.
+    :type kwargs: additional keyword arguments
 
-    Returns:
-        List[str]: a list of queries loaded from the specified collection.
+    :return: a list of queries loaded from the specified collection.
+    :rtype: List[str]
 
+    Example usage:
+    >>> log = create('mylogger')
+    >>> queries = load_pool_of_queries(log)
     """
     try:
         import ir_datasets
@@ -147,16 +169,27 @@ def build_or_load_pool_of_queries_embeddings(
     """
     Build or load embeddings for a pool of queries.
 
-    Args:
-        log (logging.Logger): logger to use for logging the process.
-        st_model (SentenceTransformer): the sentence transformer model to use for embedding the input texts.
-        queries (List[str]): the list of input queries to embed, N is the number of input queries.
-        cache_path (str, optional): path to the cache file. Defaults to "./query_pool/msmarco_gtr_embeddings.pt".
-        encode_batch_size (int, optional): batch size for encoding. Defaults to 512.
-        **kwargs: additional keyword arguments.
+    :param log: logger to use for logging the embedding process.
+    :type log: logging.Logger
+    :param st_model: the sentence transformer model to use for embedding the queries.
+    :type st_model: SentenceTransformer
+    :param queries: the list of queries to embed.
+    :type queries: List[str]
+    :param cache_path: the path to the cache file for storing query embeddings. Defaults to "./query_pool/msmarco_gtr_embeddings.pt".
+    :type cache_path: str, optional
+    :param encode_batch_size: the batch size to use for encoding the queries. Defaults to 512.
+    :type encode_batch_size: int, optional
+    :param kwargs: additional keyword arguments.
+    :type kwargs: additional keyword arguments
 
-    Returns:
-        Tuple[List[str], torch.Tensor]: a tuple containing the list of queries and their corresponding embeddings.
+    :return: a tuple containing the list of queries and their corresponding embeddings.
+    :rtype: Tuple[List[str], torch.Tensor]
+
+    Example usage:
+    >>> log = create('mylogger')
+    >>> st_model, _ = load_gtr_corrector(log)
+    >>> queries = load_pool_of_queries(log)
+    >>> queries, embeddings = build_or_load_pool_of_queries_embeddings(log, st_model, queries)
     """
     cache: Path = Path(cache_path)
     if cache.exists():
--- src/utils/mylogger.py
+++ src/utils/mylogger.py
@@ -3,6 +3,22 @@ import logging
 import os
 
 def create(name:str = 'mylogger') -> logging.Logger:
+    """
+    Create a logger with the specified name and set it up to log to a file. Formatter '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+
+    :param name: the name of the logger to create. Defaults to 'mylogger'.
+    :type name: str, optional
+
+    :return: a logger instance with the specified name, set up to log to a file.
+    :rtype: logging.Logger
+
+    Example usage:
+    >>> log = create('mylogger')
+    >>> log.info('This is an info message.')
+    2024-06-01 12:00:00,000 - mylogger - INFO - This is an info message.
+    >>> log.error('This is an error message.')
+    2024-06-01 12:00:01,000 - mylogger - ERROR - This is an error message.
+    """
     try:   
         logger = logging.getLogger(name)
         logger.setLevel(logging.DEBUG)
